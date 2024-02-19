ed933a3502bb9fdcfd1347e608dd921904b4dedb
Joao Souza <joao.souza@avenuecode.com>
2024-02-18 23:24:08 -0300

    use Env variable again

--- src/view/pages/auth/Login/LoginView.vue
+++ src/view/pages/auth/Login/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex flex-col items-center gap-4">
     <h1 class="text-2xl text-bold text-gray-900 tracking-[-1px]">Entre em sua conta</h1>
-    <p class="space-x-2" v-if="true">
+    <p class="space-x-2" v-if="activeUserRegister">
       <span class="tracking-[-0.5px] text-gray-700"> Novo por aqui ? </span>
       <router-link :to="{ name: REGISTER.name }" class="tracking-[-1px] font-medium text-teal-900">
         Crie sua conta
